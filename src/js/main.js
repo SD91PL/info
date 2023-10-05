34676f0bd3e9f673c1e5d56ea403e56a077f0157
SD91PL <sd91pl@gmail.com>
2023-10-05 13:54:43 +0200

    added script to footer__year

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const footerYear = document.querySelector('.footer__year')
+
+	const handleCurrentYear = () => {
+		const year = new Date().getFullYear()
+		footerYear.innerText = year
+	}
+
+	handleCurrentYear()
+})
