0796471169816684f7062fee4a410d94a7e90551
SD91PL <sd91pl@gmail.com>
2023-10-06 23:11:16 +0200

    nav improved

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
+	
+	//hiding nav
+	const nav = document.querySelector('.navbar-collapse')
+	document.addEventListener('click', () => {
+		if (nav.classList.contains('show')) {
+			nav.classList.remove('show')
+		}
+	})
+
 	//footer
 	const footerYear = document.querySelector('.footer__year')
-
 	const handleCurrentYear = () => {
 		const year = new Date().getFullYear()
 		footerYear.innerText = year
 	}
-
 	handleCurrentYear()
 })
